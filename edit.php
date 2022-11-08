0321914ae8f84059706f433f9bd89f091cb948f0
Aulia Akbar Khoiruddin <92700547+auliakbark@users.noreply.github.com>
2022-11-08 09:15:31 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -258,7 +258,7 @@ if (isset($_POST["Edit"])) {
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
-          <li class="nav-item">
+          <li class="nav-item menu-open">
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>
